9d5e46865505c679bfdf6ce83bc67e98de4eaf7f
Leif Warner <abimelech@gmail.com>
2011-05-12 01:33:21 -0700

    Working with no caches and just address details.

--- project/build/CalagatorMobileProject.scala
+++ project/build/CalagatorMobileProject.scala
@@ -2,7 +2,7 @@ import sbt._
 
 class CalagatorMobileProject(info: ProjectInfo) extends DefaultWebProject(info)
 {
-  val jodaTime = "joda-time" % "joda-time" % "1.6.2"
+  val time = "org.scala-tools.time" %% "time" % "0.3"
   val servletApi = "javax.servlet" % "servlet-api" % "2.5" % "provided"
   val jetty7 = "org.eclipse.jetty" % "jetty-webapp" % "7.4.0.v20110414" % "test"
 }
--- src/main/scala/org/calagator/Mobile.scala
+++ src/main/scala/org/calagator/Mobile.scala
@@ -1,18 +1,76 @@
 package org.calagator
 
 import javax.servlet.http.{HttpServletRequest, HttpServletResponse, HttpServlet}
-import javax.servlet.ServletConfig
+import java.net.URL
+
+import org.scala_tools.time.Imports._
+import xml.{NodeSeq, XML}
+import xml.dtd.{DocType, PublicID}
 
 class Mobile extends HttpServlet {
 
-  override def init(config: ServletConfig) {
-    super.init(config)
-  }
+  val parse = org.joda.time.format.ISODateTimeFormat.dateTimeNoMillis.parseDateTime _
+  def listEvents(events:NodeSeq) =
+    <ul>{
+      for (event <- events) yield
+        <li>
+          <a href={event\"id" text}>{ event\"title" text }</a>
+          <div>{
+            def displayTime(t:String) = parse(event\t text) toString "h:mma"
+            displayTime("start-time") +"-"+ displayTime("end-time") +" at "+ (event\"venue"\"title" text)
+          }</div>
+        </li>
+    }</ul>
 
   override def doGet(req:HttpServletRequest, resp:HttpServletResponse ) {
-    resp.getWriter.println("Foo-foo")
+    val today = LocalDate.now
+    val tomorrow = today + 1.day
+    val query = "http://calagator.org/events.xml?date[start]="+today+"&date[end]="+tomorrow+"&commit=Filter"
+    val calagator = XML load new URL(query)
+
+    val path = req.getPathInfo.tail
+
+    val (ongoing, current) = (calagator\"event")
+      .groupBy( (event)=> parse(event\"start-time" text).toLocalDate )
+      .partition(_._1 < today)
+
+    // filter >= tomorrow
+    val content =
+      if (path == "")
+        <div>
+          <h2>Today</h2>
+            <h3>{ today.dayOfWeek.asText }</h3>
+            { listEvents(current(today)) }
+          <h2>Tomorrow</h2>
+            <h3>{ tomorrow.dayOfWeek.asText }</h3>
+            { listEvents(current(tomorrow)) }
+        </div>
+      else
+        <div>{
+          val event = calagator\"event" filter ((e)=>(e\"id").text == path)
+          val venue = event\"venue"
+          <h4>{ event\"title" text }</h4>
+          <div>{ venue\"title" text }</div>
+          <div>{ venue\"street-address" text }</div>
+          <div>{ List("locality", "region", "postal-code") map (venue\_ text) }</div>
+        }</div>
+
+    resp setContentType "application/xhtml+xml"
+    XML.write(
+      resp.getWriter,
+      <html xmlns="http://www.w3.org/1999/xhtml">
+        <head>
+        </head>
+        <body>
+          {content}
+        </body>
+      </html>,
+      "",
+      false,
+      new DocType("html", new PublicID("-//W3C//DTD XHTML Basic 1.1//EN", "http://www.w3.org/TR/xhtml-basic/xhtml-basic11.dtd"), List())
+    )
   }
 
 }
 
-// vim: set ts=4 sw=4 et:
+// vim: set ts=2 sw=2 et:
